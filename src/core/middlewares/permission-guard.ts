b725a93921588fdbd9b433222e326c28ef44acbc
guirra-byte <guirramatheus1@gmail.com>
2025-10-26 16:33:00 -0300

    refactor: simplify permissionGuard middleware to accept request and requiredRole as parameters

--- src/core/middlewares/permission-guard.ts
+++ src/core/middlewares/permission-guard.ts
@@ -2,13 +2,9 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { AccessDeniedError } from "../errors/permission-guard-error";
 import { Role } from "@prisma/client";
 
-export function permissionGuard(requiredRole: keyof typeof Role) {
-  return async (req: FastifyRequest, res: FastifyReply) => {
-    const user = req.user;
-    if (user.role !== Role[requiredRole]) {
-      throw new AccessDeniedError();
-    }
-    
-    return res.status(200).send({ message: 'Access granted' });
-  };
+export async function permissionGuard(req: FastifyRequest, requiredRole: keyof typeof Role) {
+  const user = req.user;
+  if (user.role !== Role[requiredRole]) {
+    throw new AccessDeniedError();
+  }
 }
