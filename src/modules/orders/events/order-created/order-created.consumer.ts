43cbbd154ac766c783932bdb35fc8b72eb4efbcc
guirra-byte <guirramatheus1@gmail.com>
2025-10-26 11:10:49 -0300

    feat: implement RabbitMq consumer to listen order-create event

---
+++ src/modules/orders/events/order-created/order-created.consumer.ts
@@ -0,0 +1,17 @@
+import rabbitmqClient from '@/core/libs/rabbitmq/client';
+import { makeOrderCreated } from './make-order-created';
+
+async function orderCreatedConsumer() {
+  const { channel } = await rabbitmqClient;
+  channel.assertQueue('order.created', { durable: true });
+  channel.consume('order.created', async (message) => {
+    if (message) {
+      const { orderId } = JSON.parse(message.content.toString());
+      const orderCreatedUseCase = makeOrderCreated();
+      await orderCreatedUseCase.execute(orderId);
+      channel.ack(message);
+    }
+  });
+}
+
+orderCreatedConsumer();
